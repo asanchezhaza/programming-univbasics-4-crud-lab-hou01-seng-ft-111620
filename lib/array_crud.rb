331f3a9bf10f3e6f8bc2657f93f618e081b02a5b
Alan sanchez <asanchezhaza@gmail.com>
2020-10-08 23:05:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,19 +12,23 @@ array.push (element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+ array=[]
+ array.unshift (element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+ array=[]
+ array.pop (element)
 end
 
 def remove_element_from_start_of_array(array)
-  
+ array= []
+ array.shift (element)
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array= []
+  array.index_number[0]
 end
 
 def retrieve_first_element_from_array(array)
